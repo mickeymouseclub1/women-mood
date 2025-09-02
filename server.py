63ed351e347dc6b90540ed89ba37659e984c32ea
mickeymouseclub1 <workplease1941@outlook.com>
2025-09-02 15:27:28 +0530

    Update server.py

--- server.py
+++ server.py
@@ -15,9 +15,9 @@ ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
 # MongoDB connection
-mongo_url = os.environ['MONGO_URL']
-client = AsyncIOMotorClient(mongo_url)
-db = client[os.environ['DB_NAME']]
+#mongo_url = os.environ['MONGO_URL']
+#client = AsyncIOMotorClient(mongo_url)
+#db = client[os.environ['DB_NAME']]
 
 # Create the main app without a prefix
 app = FastAPI()
